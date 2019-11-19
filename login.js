a12c5d6b70ae923d67304385589e6255395a842e
cac0709 <cac91053@gmail.com>
2019-11-20 00:38:01 +0800

    sql login

--- login.js
+++ login.js
@@ -1,13 +1,55 @@
-function login() {
-    var username = document.getElementById("username");
-    var pass = document.getElementById("password");
-    if (username.value == "") {
-    alert("請輸入使用者名稱");
-    } else if (pass.value  == "") {
-    alert("請輸入密碼");
-    } else if(username.value == "admin" && pass.value == "123456"){
-    window.location.href="welcome.html";
-    } else {
-    alert("請輸入正確的使用者名稱和密碼！")
-    }
-    }
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '123',
+	database : 'loginjs'
+});
+
+var app = express();
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.get('/home', function(request, response) {
+	response.sendFile(path.join(__dirname + '/homepage.html'));
+});
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM logindata WHERE id = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Userid and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Userid and Password!');
+		response.end();
+	}
+
+});
+
+
+
+app.listen(3000,function () {
+	console.log("已啟動在http://localhost:3000/")
+})
+
